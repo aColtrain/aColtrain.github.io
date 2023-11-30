cc5ae838bbf0f912309adfba25edccd8e592bdb5
Coltrain <Coltrain1623@gmail.com>
2023-11-30 09:54:50 -0500

    deploy gh pages

--- my-app/src/components/navbar/Navbar.jsx
+++ my-app/src/components/navbar/Navbar.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { RiMenuLine, RiCloseLine } from 'react-icons/ri'
 import './navbar.css';
+
+//BEM --> Block Element Modifer
 
 function Navbar() {
   return (
-    <div>
-        Navbar
+    <div className="gpt3__navbar">
+        <div className="gpt3__navbar-links">
+          <div className="gpt3_navbar-links_logo">
+            {/* <img src={logo} alt="logo" /> */}
+          </div>
+
+        </div>
     </div>
   )
 }
