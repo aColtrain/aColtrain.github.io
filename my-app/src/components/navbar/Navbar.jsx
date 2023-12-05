89a3636e4f33fffad5608da1d8b7befaeeadd7b1
Coltrain <Coltrain1623@gmail.com>
2023-12-05 13:27:36 -0500

    nav bar

--- my-app/src/components/navbar/Navbar.jsx
+++ my-app/src/components/navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { RiMenuLine, RiCloseLine } from 'react-icons/ri'
+import { RiMenuLine, RiCloseLine } from 'react-icons/ri';
+import logo from '../../assets/logo.png';
 import './navbar.css';
 
 //BEM --> Block Element Modifer
@@ -9,7 +10,16 @@ function Navbar() {
     <div className="gpt3__navbar">
         <div className="gpt3__navbar-links">
           <div className="gpt3_navbar-links_logo">
-            {/* <img src={logo} alt="logo" /> */}
+            <img src={logo} alt="logo" />
+          </div>
+          <div className="gpt3__navbar-links_container">
+            <p><a href="#home">Home</a></p>
+            <p><a href="#projects">Projects</a></p>
+            <p><a href="#about">About</a></p>
+            <p><a href="#home">Contact</a></p>
+            <p><a href="#resume">Resume</a></p>
+          </div>
+
           </div>
 
         </div>
