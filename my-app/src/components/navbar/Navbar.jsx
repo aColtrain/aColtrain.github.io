4dacf06df0dc0386ff04f33c9ffe0eec2af2aa9d
Coltrain <Coltrain1623@gmail.com>
2024-02-03 22:18:44 -0500

    pt 2

--- my-app/src/components/navbar/Navbar.jsx
+++ my-app/src/components/navbar/Navbar.jsx
@@ -8,23 +8,21 @@ import './navbar.css';
 function Navbar() {
   return (
     <div className="gpt3__navbar">
-        <div className="gpt3__navbar-links">
-          <div className="gpt3_navbar-links_logo">
-            <img src={logo} alt="logo" />
-          </div>
-          <div className="gpt3__navbar-links_container">
-            <p><a href="#home">Home</a></p>
-            <p><a href="#projects">Projects</a></p>
-            <p><a href="#about">About</a></p>
-            <p><a href="#home">Contact</a></p>
-            <p><a href="#resume">Resume</a></p>
-          </div>
-
-          </div>
-
+      <div className="gpt3__navbar-links">
+        <div className="gpt3_navbar-links_logo">
+          <img src={logo} alt="logo" />
         </div>
+        <div className="gpt3__navbar-links_container">
+          <p><a href="#home">Home</a></p>
+          <p><a href="#projects">Projects</a></p>
+          <p><a href="#about">About</a></p>
+          <p><a href="#home">Contact</a></p>
+          <p><a href="#resume">Resume</a></p>
+        </div>
+      </div>
     </div>
   )
+  
 }
 
 export default Navbar
