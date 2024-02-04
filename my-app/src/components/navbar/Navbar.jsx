4ba79aad2ca107e39fcb51b58b60df8a13ba196e
Coltrain <Coltrain1623@gmail.com>
2024-02-04 00:56:23 -0500

    Nav bar set up

--- my-app/src/components/navbar/Navbar.jsx
+++ my-app/src/components/navbar/Navbar.jsx
@@ -1,28 +1,53 @@
-import React from 'react';
-import { RiMenuLine, RiCloseLine } from 'react-icons/ri';
+import React, { useState } from 'react';
+import { RiMenuLine, RiCloseLine, RiMenu3Line } from 'react-icons/ri';
 import logo from '../../assets/logo.png';
 import './navbar.css';
 
 //BEM --> Block Element Modifer
+const Menu = () => (
+  <>
+  <p><a href="#home">Home</a></p>
+  <p><a href="#projects">Projects</a></p>
+  <p><a href="#about">About</a></p>
+  <p><a href="#home">Contact</a></p>
+  <p><a href="#resume">Resume</a></p>
+  </>
+)
 
-function Navbar() {
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
   return (
     <div className="gpt3__navbar">
       <div className="gpt3__navbar-links">
-        <div className="gpt3_navbar-links_logo">
+        <div className="gpt3__navbar-links_logo">
           <img src={logo} alt="logo" />
         </div>
         <div className="gpt3__navbar-links_container">
-          <p><a href="#home">Home</a></p>
-          <p><a href="#projects">Projects</a></p>
-          <p><a href="#about">About</a></p>
-          <p><a href="#home">Contact</a></p>
-          <p><a href="#resume">Resume</a></p>
+          <Menu />
         </div>
       </div>
+      <div className="gpt3__navbar-contact">
+        <p>Contact in</p>
+        <button type="button">Contact</button>
+      </div>
+      <div className="gpt3__navbar-menu">
+        {toggleMenu
+          ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)}/> 
+          : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)}/> 
+        }
+        {toggleMenu && (
+        <div className="gpt3__navbar-menu_container scale-up-center">
+          <div className="gpt3__navbar-menu_container-links">
+            <Menu />
+            <div className="gpt3__navbar-menu_container-links-contact">
+              <p>Contact in</p>
+              <button type="button">Contact</button>
+            </div>
+          </div>
+        </div>
+        )}
+      </div>
     </div>
-  )
-  
-}
-
+  ) 
+};
 export default Navbar
