11fab3a82ceada39e75539d5c70ed153efe03efe
Coltrain <Coltrain1623@gmail.com>
2024-02-04 21:25:25 -0500

    brands

--- my-app/src/components/brand/Brand.jsx
+++ my-app/src/components/brand/Brand.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import { google, slack, linkedin } from './imports'
 import './brand.css';
 
-function Brand() {
+const Brand = () => {
   return (
-    <div>
-        Brand
+    <div className="gpt3__brand section__padding">
+      <div>
+        <img src={google} alt="google" />
+      </div>
+      <div>
+        <img src={slack} alt="slack" />
+      </div>
+      <div>
+        <img src={linkedin} alt="linkedin" height={25} />
+      </div>
     </div>
   )
 }
---
+++ my-app/src/components/brand/imports.js
@@ -0,0 +1,9 @@
+import google from '../../assets/google.png';
+import slack from '../../assets/slack.png'
+import linkedin from '../../assets/linkedin.png';
+
+export {
+    google,
+    slack,
+    linkedin,
+}
