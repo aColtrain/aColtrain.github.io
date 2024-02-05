01bb0117036db44343b20255d8767cf0dbea39e8
Coltrain <Coltrain1623@gmail.com>
2024-02-05 00:03:01 -0500

    features

--- my-app/src/containers/features/Features.jsx
+++ my-app/src/containers/features/Features.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
 import './features.css';
+import { Feature } from "../../components";
 
-function Features() {
+const featuresData = [
+  {
+    title: 'pillow',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+  },
+  {
+    title: 'Blanket',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+  },
+  {
+    title: 'Comfortor',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+  }
+]
+
+const Features = () => {
   return (
-    <div>
-        Features
+    <div className="gpt3__features section__padding" id="features">
+      <div className="gpt3__features-heading">
+        <h1 className="gradient__text">Future is now and I am tired</h1>
+        <p>Request a nice bed</p>
+      </div>
+      <div className="gpt3__features-container">
+        {featuresData.map((item, index) => (  
+          <Feature title={item.title} text={item.text} key={ item.title + index} />
+        ))}
+      </div>
     </div>
   )
 }
--- my-app/src/containers/whatGPT3/WhatGPT3.jsx
+++ my-app/src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import { Feature } from '../../components';
 import './whatGPT3.css';
 
-function WhatGPT3() {
+
+const WhatGPT3 = () => { 
   return (
-    <div>
-        WhatGPT3
+    <div className="gpt3__whatgpt3 section__margin" id="whpt3">
+      <div className="gpt3__whatgpt3-feature"> 
+        <Feature title="hi" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum" />
+      </div>
+      <div className="gpt3__whatgpt3-heading">
+        <h1 className="gradient__text">Possibilities are endless</h1>
+        <p>Explore the Library</p>
+      </div>
+      <div className="gpt3__whatgpt3-container">
+        <Feature title="one" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." />
+        <Feature title="two" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." />
+        <Feature title="three" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." />
+        
+      </div>
     </div>
   )
 }
