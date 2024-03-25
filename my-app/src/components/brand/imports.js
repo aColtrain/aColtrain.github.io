da11ad7f74440378c87526c594fe6137c2a010c9
Coltrain <Coltrain1623@gmail.com>
2024-03-25 00:46:05 -0400

    Updates

--- my-app/src/components/brand/imports.js
+++
@@ -1,9 +0,0 @@
-import google from '../../assets/google.png';
-import slack from '../../assets/slack.png'
-import linkedin from '../../assets/linkedin.png';
-
-export {
-    google,
-    slack,
-    linkedin,
-}
