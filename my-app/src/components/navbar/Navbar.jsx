72aec2df9a175b88daafc9254d39331df7d6c249
Coltrain <Coltrain1623@gmail.com>
2024-03-29 00:21:01 -0400

    spacing in navbar

--- my-app/src/components/navbar/Navbar.jsx
+++ my-app/src/components/navbar/Navbar.jsx
@@ -7,7 +7,7 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>
+    <div className="port__navbar">
       <Link to="/"> Home </Link>
       <Link to="/about"> About </Link>
       <Link to="/contact">  Contact </Link>
