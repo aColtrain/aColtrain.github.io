cd3b44edf6f308a0bacbeb083a381be3187c4c6a
Coltrain <Coltrain1623@gmail.com>
2024-08-12 23:40:56 -0400

    basic footer complete

--- my-app/src/containers/footer/Footer.jsx
+++ my-app/src/containers/footer/Footer.jsx
@@ -5,7 +5,6 @@ import { Link } from "react-router-dom";
 function Footer() {
   return (
     <div>
-        <h1> Footer </h1>
         <div className="portfolio__footer section__padding">
           <nav className="portfolio__footer-content page__width section__padding"> 
             <ul>
@@ -14,6 +13,10 @@ function Footer() {
               <li><Link to="/contact">  Contact </Link></li>
               <li><Link to="/projects"> Project </Link></li>
             </ul>
+            <div className="portfolio__footer-credits">
+              <p> Designed in <a>Figma</a> and coded in <a>Visual Studio Code 2</a> by <a>Coltrain</a>. Built with <a>React.js</a> and <a>CSS</a>, deployed with <a>Github</a>. </p>
+            </div>
+            
           </nav>
           
           <p className="copyright">© Coltrain 2024</p>
--- my-app/src/pages/HomePage.js
+++ my-app/src/pages/HomePage.js
@@ -1,31 +1,8 @@
 import React from 'react';
 import { Footer, Blog, Possibility, Skills, WhatGPT3, Header, ProjectThumbnails } from '../containers';
 import { Project } from '../components';
-import comcastThumbnail from '../assets/Comcast_MiniMart_Thumbnail1.png';
 
-// const projectTile = [
-//   // {
-//   // ProjectImg: '', 
-//   // ProjectName: '', 
-//   // ProjectDescription: '',
-//   // ProjectTags: '',
-//   // }
-//   {
-//     ProjectImg: '', 
-//     ProjectName: 'eCommerce Market', 
-//     ProjectDescription: 'Redesign of a eCommerce Market Place for small-medium sized businesses',
-//     ProjectTags: 'UX/UI Design UX/UI Research',
-    
-//   },
-//   {
-//     ProjectImg: '', 
-//     ProjectName: '', 
-//     ProjectDescription: '',
-//     ProjectTags: '',
-    
-//   }
-  
-// ]
+
 const HomePage = () => {
   return (
     <div>
@@ -38,7 +15,7 @@ const HomePage = () => {
       {/* <h1>Skills</h1> */}
       <Skills />
       {/* <h1>Whatgpt3</h1> */}
-      {/* <WhatGPT3 /> */}
+      <WhatGPT3 />
       <ProjectThumbnails />
       
     </div>
